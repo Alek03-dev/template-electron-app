469621958c98aa64b076dba3c607991960e56c10
Alek03-dev <aleksanderoliveira94@gmail.com>
2025-05-13 19:22:39 -0300

    template-app-electron

---
+++ main.js
@@ -0,0 +1,21 @@
+// Importa módulos app e BriwserWindow do Electron
+const { app, BrowserWindow } = require("electron");
+
+// Função para criar a janela principal
+function createWindow() {
+
+    // Criar uma nova instancia dp BrowserWindow
+    const window = new BrowserWindow({
+        width: 800, // Largura da janela
+        height: 600, //Altura da janela
+    })
+
+    // Carregar o arquivo HTML na janela
+    window.loadFile('index.html')
+
+}
+
+// Evento que é acionado quando Electron está pronto
+app.whenReady().then( () => {
+    createWindow(); // Chama função de criar a janela
+} )
